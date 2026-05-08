2fab6d03a0fd4877f1ec443789c65b9a3bb2edf6
Dmitrii Dolgov <9erthalion6@gmail.com>
2026-05-08 17:10:04 +0200

    Experimental reference counting
    To prevent memory leaks from allocations in helper functions, remember
    references to allocated objects. For now we assume only CString is
    reference.

--- src/worker/script.rs
+++ src/worker/script.rs
@@ -1,6 +1,7 @@
 extern crate llvm_sys as llvm;
 
 use std::{
+    cell::RefCell,
     collections::HashMap,
     fmt::Display,
     fs::OpenOptions,
@@ -132,8 +133,7 @@ pub unsafe extern "C" fn task(name: *const i8, random: bool) -> u64 {
 }
 
 thread_local! {
-    static LAST_RANDOM_PATH: RefCell<CString> =
-        RefCell::new(CString::new("").unwrap());
+    static POINTERS: RefCell<Vec<*mut i8>> = const { RefCell::new(vec![]) };
 }
 
 /// Return a randomly generated path.
@@ -151,11 +151,24 @@ pub unsafe extern "C" fn random_path(base: *const i8) -> *const i8 {
         .map(char::from)
         .collect();
 
-    LAST_RANDOM_PATH.with(|last| {
-        let mut last = last.borrow_mut();
-        *last = CString::new(format!("{base}/{uniq}")).unwrap();
-        last.as_ptr()
-    })
+    let result = CString::new(format!("{base}/{uniq}")).unwrap().into_raw();
+    POINTERS.with(|ps| ps.borrow_mut().push(result));
+    result
+}
+
+/// # Safety
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn cleanup(_: *const i8) -> u64 {
+    POINTERS.with(|ps| {
+        let mut vec = ps.borrow_mut();
+        for p in vec.as_slice() {
+            let _ = unsafe { CString::from_raw(*p) };
+        }
+
+        vec.clear();
+    });
+
+    0
 }
 
 #[derive(Debug, Clone)]
@@ -215,6 +228,16 @@ pub static RUNTIME: LazyLock<HashMap<String, RuntimeFunc>> =
                     param_count: 1,
                     param_types: &[RuntimeType::Pointer],
                     return_type: RuntimeType::Pointer,
+                },
+            ),
+            // utils
+            (
+                "cleanup".to_string(),
+                RuntimeFunc {
+                    func: cleanup as *const () as usize,
+                    param_count: 1,
+                    param_types: &[RuntimeType::Pointer],
+                    return_type: RuntimeType::Int,
                 },
             ),
         ])
@@ -478,6 +501,30 @@ impl ScriptWorker {
                     runtime_func.func as *mut c_void,
                 );
             }
+
+            // Final instruction to clear dangling pointers
+            Self::jit_instruction(
+                c"cleanup",
+                Arg::Const {
+                    text: "".to_string(),
+                },
+                &ctx,
+            );
+
+            let module_func = LLVMGetNamedFunction(
+                module,
+                c"cleanup".to_bytes().as_ptr() as *const _,
+            );
+
+            let runtime_func = &RUNTIME
+                .get("cleanup")
+                .expect("No runtime function with the name");
+
+            LLVMAddGlobalMapping(
+                ee,
+                module_func,
+                runtime_func.func as *mut c_void,
+            );
 
             // Emit a `ret i64` into the function to return the computed sum.
             let ret = LLVMConstInt(i64t, 0, 0);
